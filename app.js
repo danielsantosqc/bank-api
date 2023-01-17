e90f06b4c924fc1a9606afcfd2fd15626dabf518
Daniel Santos <danielsantosqc.dev@gmail.com>
2023-01-17 00:48:30 -0300

    add file to db connection

---
+++ app.js
@@ -0,0 +1,4 @@
+const { MongoClient } = require('mongodb');
+
+const uri = require('./atlas_uri');
+console.log(uri);
---
+++ atlas_uri.js
@@ -0,0 +1,3 @@
+const uri =
+  'mongodb+srv://student:VVydEUldzvOCDaIq@learning.u0ijyio.mongodb.net/?retryWrites=true&w=majority';
+module.exports = uri;
